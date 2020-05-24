18aa0fe6fb7c87dc3aa8d6242fad32d4b2edae5d
Glenn Cole <gcole@mac.com>
2020-05-24 14:08:51 -0700

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Glenn Cole on 5/24/20.
+//  Copyright © 2020 Glenn Cole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
